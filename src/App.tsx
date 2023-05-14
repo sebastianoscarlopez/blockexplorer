3f179abd41fce581dce1c7ed1137a44b3ef3de2b
Sebastian Oscar Lopez <sebastianoscarlopez@gmail.com>
2023-05-14 20:37:24 -0300

    feat: block basic design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import React, { useEffect, useState } from 'react';
+import { Alchemy, Network } from "alchemy-sdk";
+import React, { useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import Block from "./components/block";
-import { Html, Stats } from "@react-three/drei";
+import { OrbitControls, Stats } from "@react-three/drei";
 
-import './App.css';
+import "./App.css";
 
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
@@ -14,7 +14,6 @@ const settings = {
   network: Network.ETH_MAINNET,
 };
 
-
 // In this week's lessons we used ethers.js. Here we are using the
 // Alchemy SDK is an umbrella library with several different packages.
 //
@@ -23,7 +22,7 @@ const settings = {
 const alchemy = new Alchemy(settings);
 
 function App() {
-  const [blockNumber, setBlockNumber] = useState < number > ();
+  const [blockNumber, setBlockNumber] = useState<number>(null!);
 
   useEffect(() => {
     async function getBlockNumber() {
@@ -33,19 +32,62 @@ function App() {
     getBlockNumber();
   });
 
+  const sizeBlock = 1.0;
+  const gap = 0.005;
+
+  // Green block: #22A83F
+  // Red block: #A82222
+  // Blue block: #429FC8
   return (
-    <Canvas>
-      <ambientLight color={"red"} />
-      <pointLight color={"green"} position={[10, 10, 10]} />
-      <Block position={[-1.2, 1, 0]} />
-      <Block position={[1.2, 0, 0]} />
-      <Html>
-        <div className="App">
-          Block Number: {blockNumber}
-          {/* <CanvasContainer /> */}
-        </div>
-      </Html>
-    </Canvas>
+    <div className="App">
+      <Canvas>
+        <ambientLight intensity={2} />
+        <pointLight position={[10, 10, 10]} />
+        <Block
+          scale={sizeBlock}
+          position={[-sizeBlock - gap, 0, 0]}
+          color="#A82222"
+          blockNumber={blockNumber}
+        />
+        <Block
+          scale={sizeBlock}
+          position={[-sizeBlock - gap, sizeBlock + gap, 0]}
+          color="#A82222"
+          blockNumber={blockNumber}
+        />
+        <Block
+          scale={sizeBlock}
+          position={[0, 0, 0]}
+          color="#429FC8"
+          blockNumber={blockNumber - 1}
+        />
+        <Block
+          scale={sizeBlock}
+          position={[0, sizeBlock + gap, 0]}
+          color="#429FC8"
+          blockNumber={blockNumber - 1}
+        />
+        <Block
+          scale={sizeBlock}
+          position={[sizeBlock + gap, 0, 0]}
+          color="#22A83F"
+          blockNumber={blockNumber - 2}
+        />
+        <Block
+          scale={sizeBlock}
+          position={[sizeBlock + gap, sizeBlock + gap, 0]}
+          color="#22A83F"
+          blockNumber={blockNumber - 2}
+        />
+        {/* <Html>
+          <div className="App">
+            Block Number: {blockNumber}
+          </div>
+        </Html> */}
+        <OrbitControls rotation={[-0.5, 0, 0]} />
+        <Stats />
+      </Canvas>
+    </div>
   );
 }
 
--- src/components/block/index.tsx
+++ src/components/block/index.tsx
@@ -1,28 +1,48 @@
 import React, { useRef, useState } from "react";
 import { MeshProps, useFrame } from "@react-three/fiber";
-import { Mesh } from 'three'
+import { Text, RoundedBox } from "@react-three/drei";
 
-function Box(props: MeshProps) {
-  const ref = useRef<Mesh>(null!)
-  
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  
-  
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
+import { MeshStandardMaterial, Mesh } from "three";
+
+interface BlockProps extends MeshProps {
+  blockNumber: number;
+  color: string;
 }
 
-export default Box;
+function Block({ blockNumber, color, ...meshProps }: BlockProps) {
+  const ref = useRef<Mesh>(null!);
+
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+
+  // useFrame((state, delta) => (ref.current.rotation.x += delta * 0.3));
+
+  const blackMaterial = new MeshStandardMaterial({ color: "black" });
+
+  return (
+    <mesh
+      ref={ref}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+      {...meshProps}
+    >
+      <RoundedBox args={[1, 1, 1]} radius={0.05} creaseAngle={0.4}>
+        <meshPhysicalMaterial color={color} metalness={0.7} roughness={0.7} />
+        <Text
+          position={[0, 0, 0.501]}
+          fontSize={0.15}
+          color="cyan"
+          anchorX="center"
+          anchorY="middle"
+          outlineColor={"white"}
+          outlineWidth={0.003}
+        >
+          {blockNumber}
+        </Text>
+      </RoundedBox>
+    </mesh>
+  );
+}
+
+export default Block;
